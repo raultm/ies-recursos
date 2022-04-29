34ed1ba6b088d4bf9fdb52efc974a2fc30153144
Raul Tierno <raultm@gmail.com>
2022-04-29 11:42:36 +0200

    Educarex - Create Ps1
    Testing Mode

---
+++ 2022-04-30-educarex/educarex.ps1
@@ -0,0 +1,44 @@
+# Set-ExecutionPolicy Unrestricted -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/raultm/ies-recursos/main/2022-04-30-educarex/educarex.ps1'))
+
+function Install-EAPGTC
+{
+	$name='EAP GTC x64' 
+	$url="https://raw.githubusercontent.com/raultm/ies-recursos/main/2022-04-30-educarex/EAP-GTC-x64.msi" 
+	$path="C:\ies\EAP-GTC-x64.msi"
+
+	""
+	"  Proceso de descarga e instalacion de $name"
+    IF (Test-Path $path) {
+      '    Descarga no necesaria'
+    } ELSE {
+      "    Descargando $name..."
+      Invoke-WebRequest $url -Outfile $path
+      "    Descarga Finalizada"
+    }
+    "    Instalando $name..."
+    Start-Process $path -ArgumentList '/quiet /norestart' -Wait
+    "    Instalacion $name Finalizada."
+}
+
+function Configure-Educarex
+{
+  $name='Configuracion Educarex' 
+	$url="https://raw.githubusercontent.com/raultm/ies-recursos/main/2022-04-30-educarex/educarex.xml" 
+	$path="C:\ies\educarex.xml"
+
+	""
+	"  Proceso de descarga e instalacion de $name"
+    IF (Test-Path $path) {
+      '    Descarga no necesaria'
+    } ELSE {
+      "    Descargando $name..."
+      Invoke-WebRequest $url -Outfile $path
+      "    Descarga Finalizada"
+    }
+    "    $name en proceso..."
+    Netsh WLAN add profile filename=$path
+    "    $name Finalizada."
+}
+
+Install-EAPGTC
+Configure-Educarex
